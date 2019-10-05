38c72e78f707abc2bab31d1c72342fc18ee53338
vitorfgadelha <54113401+vitorfgadelha@users.noreply.github.com>
2019-10-05 17:20:14 -0400

    Update juntas.py

--- script/juntas.py
+++ script/juntas.py
@@ -11,8 +11,6 @@ from sensor_msgs.msg import Joy
 from time import sleep
 
 
-
-
 def mexe_junta(position_list=[pi/2,0, 0, 0, pi/2, 0], max_cont=50):
     pub = rospy.Publisher('/ur5/jointsPosTargetCommand', ManipulatorJoints, queue_size=10)
     # rospy.init_node('mexe_junta', anonymous=True)
@@ -34,23 +32,57 @@ def mexe_junta(position_list=[pi/2,0, 0, 0, pi/2, 0], max_cont=50):
 
         arg.joint_variable = position_list
         pub.publish(arg.header, arg.joint_variable)
-        print(hello_str)
-        print(cont)
+        #print(hello_str)
+        #print(cont)
         cont+=1
         rate.sleep()
 
 
+def toca_fogo1():
+    print("Zero")
+    mexe_junta()
+    print("Meio toque 1")
+    mexe_junta([pi/2,-0.225, 0, 0.3, pi/2, 0])
+    print("Meio toque 2")
+    mexe_junta([pi/2,-0.225, -pi/28, 0.3, pi/2, 0])
+    print("Meio toque 3")
+    mexe_junta([pi/2,-0.38, -pi/28, 0.3, pi/2, 0])
+    print("Toque alto")
+    mexe_junta([pi/2,-0.48, -pi/23, 0.3, pi/2, 0], max_cont=25)
+    print("Zero")
+    mexe_junta()
+    print("Meio toque 1")
+    mexe_junta([pi/2, 0, pi/1.77, 0, pi/2, 0], max_cont=25)
+    mexe_junta([pi/2,-pi/2, pi/1.7, 0, pi/2, 0])
+    print("Toque baixo")
+    mexe_junta([pi/2,-pi/2, pi/1.92, 0, pi/2, 0])
+    mexe_junta([pi/2,-pi/2, pi/1.7, 0, pi/2, 0])
+    print("Zero")
+    mexe_junta()
 
-
+def toca_fogo2():
+    print("Zero")
+    mexe_junta([pi/2, 0, 0, 0, -pi/2, 0])
+    print("Meio toque")
+    mexe_junta([pi/2, 0.225, 0, -0.3, -pi/2, 0])
+    print("Toque alto")
+    mexe_junta([pi/2, 0.225, pi/3, -0.3, -pi/2, 0], max_cont=25)
+    print("Zero")
+    mexe_junta([pi/2, 0, 0, 0, -pi/2, 0])
+    print("Meio toque")
+    mexe_junta([pi/2, 0.174533, -0.523599, 0.349066, -pi/2, 0])
+    print("Meio toque 2")
+    mexe_junta([pi/2, pi/2, -pi/1.77, 0, -pi/2, 0])
+    print("Toque baixo")
+    mexe_junta([pi/2, pi/2, -pi/2, 0, -pi/2, 0], max_cont=25)
+    print("Pos toque")
+    mexe_junta([pi/2, pi/2, -pi/1.77, 0, -pi/2, 0])
+    print("Zero")
+    mexe_junta([pi/2, 0, 0, 0, -pi/2, 0])
 
 if __name__ == '__main__':
     try:
         rospy.init_node('mexe_junta', anonymous=True)
-        mexe_junta()
-        mexe_junta([pi/2,-0.225, -pi/10, 0.3, pi/2, 0], max_cont=25)
-        mexe_junta()
-        mexe_junta([pi/2, 0, pi/1.77, 0, pi/2, 0], max_cont=25)
-        mexe_junta([pi/2,-pi/2, pi/1.77, 0, pi/2, 0])
-        mexe_junta()
+        toca_fogo1()
     except rospy.ROSInterruptException:
         pass
